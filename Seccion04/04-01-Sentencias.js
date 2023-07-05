e0faf59fa0c087a4f7a99ec56356ff7137292876
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-07-05 16:31:10 -0500

    fin seccion 4

--- Seccion04/04-01-Sentencias.js
+++ Seccion04/04-01-Sentencias.js
@@ -1,3 +1,11 @@
+let condicion = true;
+if (condicion) {
+  console.log("condicion verdadera");
+} else {
+  console.log("condicion falsa");
+}
 
-
-
+if (3 > 2)
+  console.log("condicion verdadera");
+else
+  console.log("condicion falsa");
--- Seccion04/04-04-Sentencias.js
+++ Seccion04/04-04-Sentencias.js
@@ -1,3 +1,23 @@
+/* 
+6am-11am - Buenos dias
+12pm-18pm - Buenas tardes
+19pm-24pm - Buenas noches
+0am-6am - Durmiendo
+*/
 
+let hora = 6;
+let menasaje;
 
+if (hora >= 6 && hora <= 11) {
+  menasaje = "Buenos dias";
+} else if (hora >= 12 && hora <= 18) {
+  menasaje = "Buenas tardes";
+} else if (hora >= 19 && hora <= 24) {
+  menasaje = "Buenas noches";
+} else if (hora >= 0 && hora < 6) {
+  menasaje = "Durmiendo";
+} else {
+  menasaje = "Hora desconocida";
+}
 
+console.log(menasaje);
