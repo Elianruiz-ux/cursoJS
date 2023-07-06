afb2ec34f0ece9466b83fb00bec5c40098df8fff
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-07-06 11:00:07 -0500

    fin seccion 5

---
+++ Seccion05/05-04-Ciclos.js
@@ -0,0 +1,8 @@
+for (let contador = 0; contador <= 10; contador++) {
+  if (contador % 2 == 0) {
+    console.log(contador);
+    break; // para hasta encontrar el primer resultaod
+  }
+}
+
+console.log("Fin ciclo for");
