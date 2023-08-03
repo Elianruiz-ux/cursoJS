938e5a859c11c542b99055f8a65ab8209916b2d6
Elian Joel Ruiz Callejas <elyan0811@gmail.com>
2023-08-03 10:01:31 -0500

    comienzo seccion 08

---
+++ Seccion08/08-01-Objetos.js
@@ -0,0 +1,12 @@
+let x = 10;
+console.log(x.length);
+
+let persona = {
+  nombre: "juan",
+  apellido: "perez",
+  email: "jperez@gmail.com",
+  edad: 28,
+};
+
+console.log(persona.nombre);
+console.log(persona.edad);
